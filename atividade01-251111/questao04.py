fb89417f860c7940c3a9944f2d9326e57f24ae3c
Elesfrankly <elesfrankly@gmail.com>
2025-11-12 21:06:36 -0300

    atividade do dia 11/11

---
+++ atividade01-251111/questao04.py
@@ -0,0 +1,14 @@
+#4- Calculadora de Preço Total
+#* Desenvolva um programa que calcula o preço total de uma compra. Use as seguintes informações:
+#* Nome do produto: "Cadeira Infantil"
+#* Preço unitário: R$ 12.40
+#* Quantidade: 3
+#O programa deve calcular o preço total e exibir todas as informações, incluindo o resultado final.
+
+nomeProduto = "Cadeira Infantil"
+preco = 12.40
+estoque = 3
+
+compra = preco * estoque
+
+print("Comprei",nomeProduto,"por","R$",compra)
